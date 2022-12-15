90879853b740d6b22a864cb6cddbde09fed00888
wonth93 <wongth93@gmail.com>
2022-12-14 17:14:51 -0800

    deleting appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,6 +54,21 @@ export default function Application(props) {
 
   }
 
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(setState({ ...state, appointments }));
+  }
+
   const appointment = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     const interviewers = getInterviewersForDay(state, state.day);
@@ -66,6 +81,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     )
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -12,6 +13,8 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 
 export default function Appointment(props) {
@@ -34,6 +37,15 @@ export default function Appointment(props) {
 
   }
 
+  const cancel = function() {
+    transition(DELETING);
+    Promise.all([props.cancelInterview(props.id)])
+      .then(() => transition(EMPTY))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
@@ -41,9 +53,17 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
         /> }
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SAVING && <Status message="Saving" />}
+      {mode === CONFIRM && 
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onCancel={back}
+          onConfirm={cancel}
+        />}
+      {mode === DELETING && <Status message="Deleting" />}
       {mode === CREATE &&
       <Form
         interviewers={props.interviewers}
