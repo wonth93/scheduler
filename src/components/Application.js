e227a70a245475bdb75da5d2f1e831262c73256d
wonth93 <wongth93@gmail.com>
2022-12-14 16:46:32 -0800

    create appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,6 +36,24 @@ export default function Application(props) {
 
   const appointments = getAppointmentsForDay(state, state.day);
 
+  function bookInterview(id, interview) {
+    // console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then(setState({ ...state, appointments }));
+
+  }
+
   const appointment = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     const interviewers = getInterviewersForDay(state, state.day);
@@ -47,6 +65,7 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
       />
     )
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,35 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
-const CREATE = "CREATE"
+const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 
 export default function Appointment(props) {
   
   const { mode, transition, back } = useVisualMode(props.interview? SHOW : EMPTY);
+
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    }
+
+    transition(SAVING);
+
+    Promise.all([props.bookInterview(props.id, interview)])
+      .then(() => transition(SHOW))
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
 
   return (
     <article className="appointment">
@@ -25,10 +43,12 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         /> }
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message="Saving" />}
       {mode === CREATE &&
       <Form
         interviewers={props.interviewers}
         onCancel={back}
+        onSave={save}
       />}
     </article>
   );
