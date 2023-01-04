ad8aecd8ff86cf4e0f428edba7ab77061286f269
wonth93 <wongth93@gmail.com>
2023-01-03 21:20:37 -0800

    delete overlap test

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -28,15 +28,6 @@ describe("Form", () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
-
-
-
-
-
-
-
-
-
   it("validates that the student name is not blank", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
@@ -69,46 +60,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  it.skip("calls onSave function when the name and interviewer is defined", () => {
-    /* 1. Create the mock onSave function */
-    const onSave = jest.fn();
-
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    const { queryByText, getByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} student="Lydia Miller-Jones" interviewer={interviewers[0].id} />
-    );
-
-    /* 3. Click the save button */
-    fireEvent.click(getByText("Save"));
-  
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  });
-
-  it.skip("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} interviewer={1} />
-    );
-  
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-    fireEvent.click(getByText("Save"));
-  
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-  });
-
-
-
-
-
-
-
-
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
@@ -157,6 +108,5 @@ describe("Form", () => {
   
     expect(onCancel).toHaveBeenCalledTimes(1);
   });
-
 
 });
